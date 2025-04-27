e0bfd1d20a6db52206e3848134b0ad5477fa05ca
oabuasba <156145936+oabuasba@users.noreply.github.com>
2025-04-26 22:18:11 -0700

    basic api setup

---
+++ my-react-app/src/server/api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(root_path="/")
+
+origins = [
+    "http://127.0.0.1/",
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def get_test():
+    return {"message": "CSE412 Project",
+            "members": "Omar, Samik, Zuhayr, Yousef"}    
