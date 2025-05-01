953396b6d6c9c5caee980447432ed67d25c4aa09
Zuhayr Shams <zuhayrshams@mac.3521.dhcp.asu.edu>
2025-05-01 12:09:10 -0700

    team filter

--- my-react-app/src/server/api/season.py
+++ my-react-app/src/server/api/season.py
@@ -6,7 +6,7 @@ import random
 from server.api.models import Stat_List, Stat_List_Teams_Year, Stat_List_Year, Player_Season
 #from server.api.models import Player_Season
 
-def get_player_season_stat(stat_year: Stat_List_Year, db_con: connection):
+def get_player_season_stat(stat_year: Stat_List_Teams_Year, db_con: connection):
     curs = db_con.cursor()
     player = None
     sum = " + ".join(stat_year.stat)
@@ -15,7 +15,9 @@ def get_player_season_stat(stat_year: Stat_List_Year, db_con: connection):
         query = f'''SELECT player.name, SUM({sum})
                     FROM player_season 
                     JOIN player ON player_season.p_index = player.p_index 
+                    JOIN team on team.t_idx = player_season.t_idx
                     WHERE year = '{stat_year.year}'
+                      AND abr IN '{",".join(stat_year.teams)}'
                     GROUP BY player.name;'''
         
         curs.execute(query)
